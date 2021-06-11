2159db46524dbb0f2f8bda4ea3a8a29b8ade998c
KristianMSchmidt <krms@kvuc.dk>
2021-06-11 18:49:18 +0200

    minor changes

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-#don't forget to register this app appropriately in settings.py
+# don't forget to register this app appropriately in settings.py
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY") # THis key has been exposed on github, so 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']  # new
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0']  # new
 
 # Application definition
 
