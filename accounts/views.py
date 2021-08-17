3d7fd8dbd5d6b9bdf3408fdf01c92ab93a03240f
KristianMSchmidt <krms@kvuc.dk>
2021-08-17 14:51:49 +0200

    profile update now working with htmx

--- accounts/views.py
+++ accounts/views.py
@@ -5,21 +5,23 @@ from .forms import CustomUserCreationForm, UserUpdateForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+
 class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
 
 @login_required
 def profile_view(request):
     if request.method == 'POST':
         form = UserUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
-            u = form.save(commit=False)   
+            u = form.save(commit=False)
             u.save(actor=request.user)
             messages.success(
                 request, f'Your account has been updated!')
-            return redirect('profile')  
+            return redirect('profile')
     else:
         form = UserUpdateForm(instance=request.user)
 
